88741e2f007d6a412e0e0cbc4293c43e39a59e43
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-07-15 17:56:31 +0300

    Improved logic of code_labels function

--- src/features/features.py
+++ src/features/features.py
@@ -1,16 +1,13 @@
-from typing import Optional
 import pandas as pd
 
 
-def code_labels(data: pd.DataFrame,
-                col_name: Optional[str] = None) -> pd.DataFrame:
+def code_labels(data: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a new column to a dataframe with all labels encoded into numbers
     """
     df = data.copy()
     df.label = pd.Categorical(df.label, df.label.unique())
     lbl_col_idx = df.columns.get_loc('label') # index of label column
-    new_col_name = col_name if col_name else 'lbl_code'
-    df.insert(lbl_col_idx + 1, new_col_name, df.label.cat.codes)
+    df.insert(lbl_col_idx + 1, 'lbl_code', df.label.cat.codes)
     return df
 
