f92f8b1bb42b7ebc4faf8d253f628c45dc6cd005
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-07-25 21:04:05 +0300

    Improved logic of cli parameters

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,29 +8,23 @@ from ..features.features import encode_labels
 def create_parser() -> ArgumentParser:
     """ Initializes parser """
     parser = ArgumentParser()
-    parser.add_argument('raw_path', help='path to raw .csv file with glyphs')
+    parser.add_argument('glyphs_path',
+                        help='path to the cleaned .csv file with glyphs')
     parser.add_argument('mnist_path',
-                         help='path to raw .csv file with mnist')
-    parser.add_argument('out_path', help='path to final .csv file')
+                        help='path to the raw .csv file with mnist')
     return parser
 
 
-def make_dataset(raw_path: str, mnist_path: str, out_path: str) -> None:
+def make_dataset(glyphs_path: str, mnist_path: str) -> None:
     """ Generates a complete dataset """
-    raw_df = pd.read_csv(raw_path)
-    cols_to_drop = ['transliter_kmu2010', 'name', 'type', 'is_alternate',
-                    'top', 'bottom', 'left', 'right', 'height', 'width']
-    raw_df.drop(cols_to_drop, axis='columns', inplace=True)
-    raw_df.replace({False: 0, True: 1}, inplace=True)
+    glyphs_df = pd.read_csv(glyphs_path)
     mnist_df = pd.read_csv(mnist_path)
-    completed_df = pd.concat((mnist_df, raw_df), ignore_index=True)
+    completed_df = pd.concat((mnist_df, glyphs_df), ignore_index=True)
     completed_df = encode_labels(completed_df)
-    completed_df.to_csv(out_path, index=False)
-    raw_df = encode_labels(raw_df)
-    raw_df.to_csv('data/interim/data_cleaned.csv', index=False)
-
+    completed_df.to_csv('data/processed/final.csv', index=False)
+    
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    make_dataset(args.raw_path, args.mnist_path, args.out_path)
+    make_dataset(args.glyphs_path, args.mnist_path)
--- src/data/prepare_mnist.py
+++ src/data/prepare_mnist.py
@@ -8,13 +8,14 @@ from torchvision.datasets import MNIST
 def create_parser() -> ArgumentParser:
     """ Initializes parser """
     parser = ArgumentParser()
+    parser.add_argument('raw_dir', help='root dir where MNIST/raw exists')
+    parser.add_argument('out_path', help='path to .csv file')
     parser.add_argument('num_samples', type=int,
                         help='number of samples per class')
-    parser.add_argument('out_path', help='path to .csv file')
     return parser
 
 
-def prepare_mnist(num_samples: int, out_path: str) -> None:
+def prepare_mnist(raw_dir: str, out_path: str, num_samples: int, ) -> None:
     """
     Prepares `num_samples` samples of each class to be joined with original
     data.
@@ -22,9 +23,7 @@ def prepare_mnist(num_samples: int, out_path: str) -> None:
     Each sample is saved as an image, and corresponding information is added\
  to the `out_name`.csv file
     """
-    data = MNIST(
-        'data/raw', download=True, train=True, transform=T.Grayscale(3)
-    )
+    data = MNIST(raw_dir, download=True, train=True, transform=T.Grayscale(3))
     targets = np.array(data.targets)
     indices = (np.where(targets == cls)[0][:num_samples] for cls in range(10)) 
     with open(out_path, 'w', encoding='utf-8') as f:
@@ -40,5 +39,5 @@ def prepare_mnist(num_samples: int, out_path: str) -> None:
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    prepare_mnist(args.num_samples, args.out_path)
+    prepare_mnist(args.raw_dir, args.out_path, args.num_samples)   
 
