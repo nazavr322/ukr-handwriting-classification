108e2302fa84ee17918ef4913230b61e2885e110
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-07-28 18:03:20 +0300

    Added parameter to specify path to img dir

--- src/data/prepare_mnist.py
+++ src/data/prepare_mnist.py
@@ -10,13 +10,17 @@ def create_parser() -> ArgumentParser:
     """ Initializes parser """
     parser = ArgumentParser()
     parser.add_argument('raw_dir', help='root dir where MNIST/raw exists')
-    parser.add_argument('out_path', help='path to .csv file')
     parser.add_argument('num_samples', type=int,
                         help='number of samples per class')
+    parser.add_argument('out_path', help='path to .csv file')
+    parser.add_argument('out_img_dir',
+                        help='target dir where images will be stored')
     return parser
 
 
-def prepare_mnist(raw_dir: str, out_path: str, num_samples: int) -> None:
+def prepare_mnist(
+    raw_dir: str, num_samples: int, out_path: str, out_img_dir: str
+) -> None:
     """
     Prepares `num_samples` samples of each class to be joined with original
     data.
@@ -25,8 +29,8 @@ def prepare_mnist(raw_dir: str, out_path: str, num_samples: int) -> None:
  to the `out_name`.csv file
     """
     # create glyphs directory if it doesn't exist
-    if not os.path.isdir('data/processed/glyphs'):
-        os.mkdir('data/processed/glyphs')
+    if not os.path.isdir(out_img_dir):
+        os.mkdir(out_img_dir)
     
     data = MNIST(raw_dir, download=True, train=True, transform=T.Grayscale(3))
     targets = np.array(data.targets)
@@ -37,13 +41,15 @@ def prepare_mnist(raw_dir: str, out_path: str, num_samples: int) -> None:
         for cls_indices in indices:
             for i, cls_idx in enumerate(cls_indices):
                 img, cls = data[cls_idx]
-                filename = f'glyphs/{cls}-{i}.png'
-                img.save(os.path.join('data/processed', filename))
-                f.write(f'\n{cls},{False},{filename}')
+                filename = f'{cls}-{i}.png'
+                img.save(os.path.join(out_img_dir, filename))
+                f.write(f'\n{cls},{False},glyphs/{filename}')
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    prepare_mnist(args.raw_dir, args.out_path, args.num_samples)   
+    prepare_mnist(
+        args.raw_dir, args.num_samples, args.out_path, args.out_img_dir
+    )   
 
