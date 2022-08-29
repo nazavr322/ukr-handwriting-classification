c91e6db068952daec0be6281b128416e94d84016
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-08-29 18:22:24 +0300

    Changed canvas size

--- src/frontend/Home.py
+++ src/frontend/Home.py
@@ -1,10 +1,11 @@
 from math import ceil
 
+import requests
 import cv2 as cv
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-from utils import create_bounding_boxes, merge_bounding_boxes 
+from utils import create_bounding_boxes, merge_bounding_boxes, encode_image
 
 
 st.title('Classification of ukrainian handwriting 📝')
@@ -17,8 +18,8 @@ with left_col:
         stroke_width=5,
         update_streamlit=False,
         background_color="rgb(255, 255, 255)",
-        height=300,
-        width=300,
+        height=200,
+        width=200,
     )
 
 canvas_img = canvas_result.image_data[:, :, :3].copy()
@@ -33,9 +34,18 @@ if len(canvas_img_metadata) == 1:
     left, top = meta['left'], meta['top']
     width, height = meta['width'], meta['height']
     p1 = (int(left), int(top))
-    p2 = (ceil(left + width) + 4, ceil(top + height) + 4)
+    p2 = (ceil(left + width) + 5, ceil(top + height) + 5)
 else:
     boxes = create_bounding_boxes(canvas_img_metadata)    
     p1, p2 = merge_bounding_boxes(boxes)
-cv.rectangle(canvas_img, p1, p2, (255, 0, 0), 2)
-right_col.image(canvas_img, 'Here is your prediction!')
+img_with_bb = cv.rectangle(canvas_img.copy(), p1, p2, (255, 0, 0), 2)
+right_col.image(img_with_bb, 'Here is your prediction!')
+
+cropped_img = canvas_img[p1[1]:p2[1], p1[0]:p2[0]]
+padded_img = cv.copyMakeBorder(
+    cropped_img, 5, 5, 5, 5, cv.BORDER_CONSTANT, value=[255, 255, 255]
+)
+encoded_img = encode_image(padded_img)
+file = {'img_file': ('test_img.png', encoded_img)}
+response = requests.post('http://127.0.0.1:8000/file', files=file)
+st.write(response.json())
