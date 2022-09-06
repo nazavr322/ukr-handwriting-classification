d29f464ac4e4e902e438311f89d8330c51937b4e
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-09-06 14:28:59 +0300

    Added predict route

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,18 +1,29 @@
-import numpy as np
-import cv2 as cv
 from fastapi import FastAPI, UploadFile
+from pydantic import BaseModel, conlist
+
+from .utils import read_img, InferenceModel
+
+
+class Prediction(BaseModel):
+    """Response model for prediction"""
+    label_logits: conlist(float, min_items=43, max_items=43)
+    is_uppercase_logits: float
 
 
 app = FastAPI()
+
+@app.on_event('startup')
+def load_model():
+    global model
+    model = InferenceModel()
 
 @app.get('/')
 def root():
     return {}
 
-@app.post('/file/')
-def save_file(img_file: UploadFile):
-    img = np.frombuffer(img_file.file.read(), np.uint8)
-    img = cv.imdecode(img, cv.IMREAD_UNCHANGED)
-    status = cv.imwrite(img_file.filename, img)
-    msg = 'Your image succesfully saved' if status else 'Error occured'
-    return {'status': msg}
+@app.post('/predict/', response_model=Prediction)
+def predict(img_file: UploadFile):
+    image = read_img(img_file)
+    lbl_logits, is_upp_logits = model.predict(image)
+    return lbl_logits.tolist()[0], is_upp_logits.item() 
+
