31fedbe51f50b28f102f4a0b81a1340c826445e8
Nazar Polishchuk <nazarr.polishchuk@gmail.com>
2022-11-12 01:17:34 +0200

    Added collection of user input into S3 bucket

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,10 @@
+import os
+
+from boto3.session import Config
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel, conlist
 
-from .utils import read_img, InferenceModel
+from .utils import read_img, InferenceModel, send_to_bucket
 
 
 class Prediction(BaseModel):
@@ -12,6 +15,19 @@ class Prediction(BaseModel):
 
 
 app = FastAPI()
+
+BUCKET_NAME = os.environ['USER_INPUT_BUCKET_NAME']
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+S3_ENDPOINT_URL = os.environ['MLFLOW_S3_ENDPOINT_URL']
+CLIENT_ARGS = {
+    'endpoint_url': S3_ENDPOINT_URL,
+    'aws_access_key_id': AWS_ACCESS_KEY,
+    'aws_secret_access_key': AWS_SECRET_KEY,
+    'aws_session_token': None,
+    'config': Config(signature_version='s3v4'),
+    'verify': False,
+}
 
 
 @app.on_event('startup')
@@ -27,7 +43,9 @@ def root():
 
 @app.post('/predict/', response_model=Prediction)
 def predict(img_file: UploadFile):
-    image = read_img(img_file)
+    byte_img = img_file.file.read()
+    send_to_bucket(byte_img, BUCKET_NAME, CLIENT_ARGS)
+    image = read_img(byte_img)
     lbl_logits, is_upp_logits = model.predict(image)
     return {
         'label_logits': lbl_logits.tolist()[0],
--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,7 +1,10 @@
+import uuid
+from typing import Optional
+
 import mlflow.pytorch
 import numpy as np
+import boto3
 import cv2 as cv
-from fastapi import UploadFile
 from torch import Tensor, from_numpy, device
 
 from ..models.models import HandwritingClassifier
@@ -27,13 +30,13 @@ class InferenceModel:
         return self.model(images)
 
 
-def read_img(img_file: UploadFile) -> Tensor:
+def read_img(byte_img: bytes) -> Tensor:
     """
     Reads image from buffer and returns it as `torch.Tensor` that you can pass
     directly to a model.
     """
     # read image from buffer
-    image = np.frombuffer(img_file.file.read(), np.uint8)
+    image = np.frombuffer(byte_img, np.uint8)
     image = cv.imdecode(image, cv.IMREAD_UNCHANGED)
     image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     image = cv.resize(image, (28, 28))
@@ -42,3 +45,19 @@ def read_img(img_file: UploadFile) -> Tensor:
     # normalize
     image = (image - MEAN) / STD
     return from_numpy(image.transpose(2, 0, 1)[np.newaxis])
+
+
+def send_to_bucket(
+    byte_img: bytes,
+    bucket_name: str,
+    client_args: dict,
+    filename: Optional[str] = None,
+) -> None:
+    """
+    Uploads image as bytes to S3 bucket named `bucket_name`. If
+    filename is not specified a random UUID4 is generated.
+    """
+    file_name = filename if filename is not None else f'{uuid.uuid4()}.png'
+    s3 = boto3.resource('s3', **client_args)
+    bucket = s3.Bucket(bucket_name)
+    bucket.put_object(Body=byte_img, Key=file_name, ContentType='image/png')
